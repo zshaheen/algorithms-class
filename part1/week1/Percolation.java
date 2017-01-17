138dc2fcba5ecb42053d9051e9f59d89a542a4f2
Zeshawn <zshaheen93@gmail.com>
2017-01-16 19:16:06 -0800

    More cleanup

--- part1/week1/Percolation.java
+++ part1/week1/Percolation.java
@@ -1,49 +1,48 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.io.*;
-import java.util.Scanner;
+import edu.princeton.cs.algs4.In;
 
-//javac-algs4 Percolation.java
-//java-algs4 Percolation
+// javac-algs4 Percolation.java
+// java-algs4 Percolation
 public class Percolation {
-  private int N;
-  public int[][] id;
+  private int nValue;
+  private int[][] id;
   private WeightedQuickUnionUF uf;
 
   public Percolation(int n) {
     // create n-by-n grid, with all sites blocked
-    N = n;
+    nValue = n;
     uf = new WeightedQuickUnionUF(n*n);
     id = new int [n][n];
 
-    for(int i = 0; i < n; i++) {
-      for(int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
         id[i][j] = 0;
       }
     }
   }
 
   private boolean connected(int row1, int col1, int row2, int col2) {
-    return uf.connected(row1*N+col1, row2*N+col2);
+    return uf.connected(row1*nValue+col1, row2*nValue+col2);
   }
 
   private void union(int row1, int col1, int row2, int col2) {
-      uf.union(row1*N+col1, row2*N+col2);
+      uf.union(row1*nValue+col1, row2*nValue+col2);
   }
 
-  private void check_values(int row, int col) {
-    if(row<=0 || row > N)
+  private void checkValues(int row, int col) {
+    if (row <= 0 || row > nValue)
       throw new java.lang.IndexOutOfBoundsException();
-    if(col<=0 || col > N)
+    if (col <= 0 || col > nValue)
       throw new java.lang.IndexOutOfBoundsException();
   }
 
-  private boolean check_values_boolean(int row, int col) {
+  private boolean checkValuesBoolean(int row, int col) {
     int r = row-1;
     int c = col-1;
     try {
-      int i = id[r][c];
+      r = id[r][c];
       return true;
-    } catch(ArrayIndexOutOfBoundsException e) {
+    } catch (ArrayIndexOutOfBoundsException e) {
       return false;
     }
   }
@@ -51,21 +50,21 @@ public class Percolation {
   public void open(int row, int col) {
     int[] values = {row-1, col, row+1, col, row, col-1, row, col+1};
 
-    check_values(row, col);
+    checkValues(row, col);
     id[row-1][col-1] = 1;
 
-    for(int i=0; i<values.length; i=i+2) {
+    for (int i = 0; i < values.length; i = i + 2) {
       int r = values[i];
       int c = values[i+1];
 
-      if(check_values_boolean(r, c) && isOpen(r, c) && !connected(row-1, col-1, r-1, c-1))
+      if (checkValuesBoolean(r, c) && isOpen(r, c) && !connected(row-1, col-1, r-1, c-1))
             union(row-1, col-1, r-1, c-1);
     }
   }
 
   public boolean isOpen(int row, int col) {
     // is site (row, col) open?
-    check_values(row, col);
+    checkValues(row, col);
 
     try {
       if (id[row-1][col-1] == 0)
@@ -73,7 +72,7 @@ public class Percolation {
       else
         return true;
     }
-    catch(ArrayIndexOutOfBoundsException exception) {
+    catch (ArrayIndexOutOfBoundsException exception) {
       return false;
     }
   }
@@ -84,12 +83,12 @@ public class Percolation {
     //  via a chaining of neighbors (l,r,u,d) open sites.
 
     // First check if this is an open site
-    if (isOpen(row, col) == false)
+    if (!isOpen(row, col))
       return false;
 
     // Check all open sites on the top row and see if it's connected to row, col.
-    for(int i=1; i<=N; i++) {
-      if(isOpen(1, i) && connected(row-1, col-1, 1-1, i-1))
+    for (int i = 1; i <= nValue; i++) {
+      if (isOpen(1, i) && connected(row-1, col-1, 1-1, i-1))
         return true;
     }
     return false;
@@ -103,8 +102,8 @@ public class Percolation {
   public boolean percolates() {
     // does the system percolate?
     // percolates if there is a full site on the bottom row
-    for(int i=1; i<=N; i++) {
-      if(isFull(N, i))
+    for (int i = 1; i <= nValue; i++) {
+      if (isFull(nValue, i))
         return true;
     }
     return false;
@@ -113,24 +112,21 @@ public class Percolation {
   public static void main(String[] args) {
     // test client (optional)
     try {
-      FileInputStream fstream = new FileInputStream(args[0]);
-      BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-      String strLine;
+      In in = new In(args[0]);
+      Percolation p = new Percolation(in.readInt());
+      int row, col;
 
-      Percolation p = new Percolation(Integer.parseInt(br.readLine()));
-
-      while ((strLine = br.readLine()) != null) {
-        Scanner in = new Scanner(strLine).useDelimiter("[^0-9]+");
-        int row = in.nextInt();
-        int col = in.nextInt();
+      while(!in.isEmpty()) {
+        row = in.readInt();
+        col = in.readInt();
         p.open(row, col);
       }
-      br.close();
-      System.out.println(args[0]);
-      System.out.println(p.percolates());
 
-    } catch(IOException ex) {
-      System.out.println("File not found or IO exception.");
+      // System.out.println(args[0]);
+      // System.out.println(p.percolates());
+
+    } catch(IllegalArgumentException e) {
+      System.out.println(e);
     }
   }
 }
