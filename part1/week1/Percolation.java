b7c2204a5946ddcda49bec13f6db5b1cac317ea0
Zeshawn <zshaheen93@gmail.com>
2017-01-16 16:43:44 -0800

    cleanup

--- part1/week1/Percolation.java
+++ part1/week1/Percolation.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.util.*;
+import java.io.*;
+import java.util.Scanner;
 
 //javac-algs4 Percolation.java
 //java-algs4 Percolation
@@ -19,7 +20,6 @@ public class Percolation {
         id[i][j] = 0;
       }
     }
-
   }
 
   private boolean connected(int row1, int col1, int row2, int col2) {
@@ -35,7 +35,6 @@ public class Percolation {
       throw new java.lang.IndexOutOfBoundsException();
     if(col<=0 || col > N)
       throw new java.lang.IndexOutOfBoundsException();
-
   }
 
   private boolean check_values_boolean(int row, int col) {
@@ -49,44 +48,6 @@ public class Percolation {
     }
   }
 
-  //DELETE THIS
-  /*
-  private ArrayList listOfValuesToCheck(int row, int col) {
-    //int[] values = new int[8];
-    int[] values = {row-1, col, row+1, col, row, col-1, row, col+1};
-    ArrayList good_values = new ArrayList();
-    try {
-      int i = id[values[0]][values[1]];
-      good_values.add(values[0]+1);
-      good_values.add(values[1]+1);
-    } catch(ArrayIndexOutOfBoundsException e) {
-      ;
-    }
-    try {
-      int i = id[values[2]][values[3]];
-      good_values.add(values[2]+1);
-      good_values.add(values[3]+1);
-    } catch(ArrayIndexOutOfBoundsException e) {
-      ;
-    }
-    try {
-      int i = id[values[4]][values[5]];
-      good_values.add(values[4]+1);
-      good_values.add(values[5]+1);
-    } catch(ArrayIndexOutOfBoundsException e) {
-      ;
-    }
-    try {
-      int i = id[values[6]][values[7]];
-      good_values.add(values[6]+1);
-      good_values.add(values[7]+1);
-    } catch(ArrayIndexOutOfBoundsException e) {
-      ;
-    }
-    return good_values;
-  }
-
-  */
   public void open(int row, int col) {
     int[] values = {row-1, col, row+1, col, row, col-1, row, col+1};
 
@@ -97,113 +58,15 @@ public class Percolation {
       int r = values[i];
       int c = values[i+1];
 
-      if(check_values_boolean(r, c)) {
-        //if(isOpen(row, col)) {
-        if(isOpen(r, c)) {
-          /*System.out.println(row);
-          System.out.println(col);
-          System.out.println(r-1);
-          System.out.println(c-1);
-          System.out.println("-------");*/
-          if (connected(row-1, col-1, r-1, c-1)==false) {
-            //System.out.println("union called");
+      if(check_values_boolean(r, c) && isOpen(r, c) && !connected(row-1, col-1, r-1, c-1))
             union(row-1, col-1, r-1, c-1);
-          }
-        }
-      }
     }
-
-
   }
-
-  /*
-  private void less_old_open(int row, int col) {
-    ArrayList values = listOfValuesToCheck(row-1, col-1);
-
-    for(int i=0; i<values.size(); i=i+2) {
-      int r = values.get(i);
-      int c = values.get(i+1);
-
-      if(isOpen(row, col)) {
-        if (connected(row, col, r-1, c-1)==false) {
-          //System.out.println("union called");
-          union(row, col, r-1, c-1);
-        }
-      }
-    }
-
-  }*/
-
-  private void old_open(int row, int col) {
-
-    //Check the sites around row, col and if they are open, connect them
-    //right
-    //try {
-      if(isOpen(row, col+1)) {
-        if (connected(row-1, col-1, row-1, col-1+1)==false) {
-          System.out.println("union called");
-          union(row-1, col-1, row-1, col-1+1);
-        }
-      }
-    /*} catch(IndexOutOfBoundsException e) {
-      ;
-    }*/
-
-    //left
-    //try {
-      if(isOpen(row, col-1)) {
-        if(connected(row-1, col-1, row-1, col-1-1)==false) {
-          System.out.println("union called");
-          union(row-1, col-1, row-1, col-1-1);
-        }
-      }
-    /*} catch(IndexOutOfBoundsException e) {
-      ;
-    }*/
-
-    //down
-    //try {
-      if(isOpen(row+1, col)) {
-        if(connected(row-1, col-1, row-1+1, col-1)==false) {
-          System.out.println("union called");
-          union(row-1, col-1, row-1+1, col-1);
-        }
-      }
-    /*} catch(IndexOutOfBoundsException e) {
-      ;
-    }*/
-
-
-    //up
-    //try {
-      if(isOpen(row-1, col)) {
-        if(connected(row-1, col-1, row-1-1, col-1)==false) {
-          System.out.println("union called");
-          union(row-1, col-1, row-1-1, col-1);
-        }
-      }
-    /*} catch(IndexOutOfBoundsException e) {
-      ;
-    }*/
-
-      // open site (row, col) if it is not open already
-      id[row-1][col-1] = 1;
-  }
-
-  //IMPORTANT NOTE: ANYTHING THAT DOES *NOT* INTERFACE WITH THE PUBLIC NEEDS TO BE DECREASED BY 1
-  // OTHERW
-
-  //TODO
-  //have isOpen work with values from 1-n [x]
-  //make open() give valid commands to isOpen so that there are no exceptions[x]
-  //check that percolate() works with new schime (1-n)[x]
-  //check isFull() for the third time [x]
 
   public boolean isOpen(int row, int col) {
     // is site (row, col) open?
     check_values(row, col);
 
-    //System.out.println("passed check_values()");
     try {
       if (id[row-1][col-1] == 0)
         return false;
@@ -249,26 +112,25 @@ public class Percolation {
 
   public static void main(String[] args) {
     // test client (optional)
-    Percolation p = new Percolation(3);
-    //Throw a java.lang.IndexOutOfBoundsException
-    //p.open(0,0); //java.lang.ArrayIndexOutOfBoundsException
-    //p.isOpen(0,0);
-    //p.isFull(0,0);
+    try {
+      FileInputStream fstream = new FileInputStream(args[0]);
+      BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+      String strLine;
 
-    //System.out.println(Arrays.deepToString(p.id));
-    //System.out.println(p.isOpen(1,1));
-    p.open(1,1);
+      Percolation p = new Percolation(Integer.parseInt(br.readLine()));
 
-    //System.out.println(Arrays.deepToString(p.id));
-    //System.out.println(p.isFull(3,1));
+      while ((strLine = br.readLine()) != null) {
+        Scanner in = new Scanner(strLine).useDelimiter("[^0-9]+");
+        int row = in.nextInt();
+        int col = in.nextInt();
+        p.open(row, col);
+      }
+      br.close();
+      System.out.println(args[0]);
+      System.out.println(p.percolates());
 
-    //p.open(3,1);
-    p.open(2,1);
-
-    //System.out.println(p.isFull(3,1));
-    p.open(3,1);
-    //System.out.println(p.isFull(3, 2));
-    //Why does open(1,1), open(2,1), open(3,2) work?
-    System.out.println(p.percolates());
+    } catch(IOException ex) {
+      System.out.println("File not found or IO exception.");
+    }
   }
 }
